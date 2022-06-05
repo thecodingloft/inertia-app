b3d222a5ce966e3a349eb69d7747f36bbc613297
Samuel <samuelschanderl@Samuels-MacBook-Pro.local>
2022-06-05 18:47:28 +0200

    WIP: next - loading spinner

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Hello {{ name }}</h1>
+    <Nav></Nav>
+</template>
+
+<script>
+
+import { Link } from '@inertiajs/inertia-vue3';
+import Nav from '../Shared/Nav.vue'
+
+export default{
+    components: {
+        Link,
+        Nav
+    }
+};
+</script>
+
+<style>
+
+</style>
